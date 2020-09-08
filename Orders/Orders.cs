4659a6a25a80e355e2e1a3507f50420631a9a62a
Reiysher <reiysher25@gmail.com>
2020-09-08 14:44:42 +0300

    Добавьте файлы проекта.

---
+++ Orders/Orders.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Orders
+{
+    enum Status
+    {
+        Обрабатывается,
+        Оплачен,
+        Доставлен
+    }
+    class Orders
+    {
+        public int id;
+        public double price;
+        public DateTime date;
+        public Status st;
+
+        public Orders(int id, double price, DateTime date, Status st)
+        {
+            this.id = id;
+            this.price = price;
+            this.date = date;
+            this.st = st;
+        }
+
+        public void GetOrdersInfo()
+        {
+            Console.Clear();
+            Console.WriteLine($"Заказ №{id}\n ID: {id}\n Цена: {price}\n Дата заказа: {date}\n Статус заказа: {st}");
+            Console.WriteLine("Чтобы выйти в главное меню нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+
+        public void GetFullInfo()
+        {
+            Console.WriteLine($"ID: {id}\t Цена: {price}\t Дата заказа: {date}\t Статус заказа: {st}");
+        }
+    }
+}
---
+++ Orders/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Orders
+{
+
+    class Program
+    {
+
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                int choice = 0;
+
+                Status processed = Status.Обрабатывается;
+                Status paid = Status.Оплачен;
+                Status delivered = Status.Доставлен;
+
+                DateTime date1 = new DateTime(2020, 7, 25, 12, 45, 15);
+                DateTime date2 = new DateTime(2020, 7, 17, 10, 30, 49);
+                DateTime date3 = new DateTime(2020, 7, 12, 17, 22, 59);
+                DateTime date4 = new DateTime(2020, 6, 29, 15, 16, 23);
+                DateTime date5 = new DateTime(2020, 6, 25, 14, 50, 36);
+
+                Orders[] orders = new Orders[5];
+                orders[0] = new Orders(1, 500.50, date1, processed);
+                orders[1] = new Orders(2, 346.79, date2, paid);
+                orders[2] = new Orders(3, 17643.2, date3, delivered);
+                orders[3] = new Orders(4, 2131.43, date4, delivered);
+                orders[4] = new Orders(5, 1216.21, date5, delivered);
+
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Отобразить информацию про заказ по id");
+                Console.WriteLine("2. Вывести информацию о всех заказах");
+                Console.WriteLine("3. Выход");
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    // Console.WriteLine("Вы ввели не число!");
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        OrderId(orders);
+                        break;
+                    case 2:
+                        OrderFullInfo(orders);
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка ввода, нажмите любую клавишу.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        static void OrderId(Orders[] orders)
+        {
+            int id;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Введите ID заказа: ");
+
+                try
+                {
+                    id = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Такого ID не существует, нажмите любую клавишу.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+
+                switch (id)
+                {
+                    case 1:
+                        orders[0].GetOrdersInfo();
+                        return;
+                    case 2:
+                        orders[1].GetOrdersInfo();
+                        return;
+                    case 3:
+                        orders[2].GetOrdersInfo();
+                        return;
+                    case 4:
+                        orders[3].GetOrdersInfo();
+                        return;
+                    case 5:
+                        orders[4].GetOrdersInfo();
+                        return;
+                    default:
+                        Console.WriteLine("Такого ID не существует, нажмите любую клавишу.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+
+            }
+        }
+
+        static void OrderFullInfo(Orders[] orders)
+        {
+            Console.Clear();
+            for(int i = 0; i < orders.Length; i++)
+            {
+                orders[i].GetFullInfo();
+            }
+            Console.WriteLine("Чтобы выйти в главное меню нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+    }
+}
