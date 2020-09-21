df7422570a7da8a101508b2a16fbd0f7fb3fe91a
Reiysher <reiysher25@gmail.com>
2020-09-21 19:14:54 +0300

    Rewrited date generator
    renamed quantuty parameter
    variables now not in cycle

--- MerchantClassLibraries/OrdersManager.cs
+++ MerchantClassLibraries/OrdersManager.cs
@@ -7,16 +7,38 @@ namespace MerchantClassLibraries
 {
     public class OrdersManager
     {
-        public static List<Order> GenerateOrders(int a)
+        public static List<Order> GenerateOrders(int quantity)
         {
             List<Order> orders = new List<Order>();
-            for(int i = 0; i <= a; i++)
+            Random rnd = new Random();
+            int year;
+            int month;
+            int day;
+
+            for (int i = 0; i <= quantity; i++)
             {
-                Random rnd = new Random(i);
-                decimal generatePrice = rnd.Next(0, 100000);
-                DateTime generateDate = new DateTime(2020, rnd.Next(1, 12), rnd.Next(1, 30), rnd.Next(0, 23), rnd.Next(0, 59), rnd.Next(0, 59));
-                Status generateStatus = (Status)rnd.Next(0, 3);
-                orders.Add(new Order(i, generatePrice, generateDate, generateStatus));
+                decimal generatedPrice = (decimal)rnd.Next(0, 10000000) / 100;
+                year = rnd.Next(2010, 2021);
+                month = rnd.Next(1, 13);
+                if (year % 4 == 0 && month == 2)
+                {
+                    day = rnd.Next(1, 30);
+                }
+                else if(year % 4 != 0 && month == 2)
+                {
+                    day = rnd.Next(1, 29);
+                }
+                else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                {
+                    day = rnd.Next(1, 32);
+                }
+                else
+                {
+                    day = rnd.Next(1, 31);
+                }
+                DateTime generatedDate = new DateTime(year, month, day, rnd.Next(0, 24), rnd.Next(0, 60), rnd.Next(0, 60));
+                
+                orders.Add(new Order(i, generatedPrice, generatedDate, (Status)rnd.Next(0, 3)));
             }
 
             return orders;
