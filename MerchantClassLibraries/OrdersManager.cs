49837c849307a0759f92b10d80d5ab14cfcf5da7
Reiysher <reiysher25@gmail.com>
2020-09-21 21:12:01 +0300

    rewriting if...else

--- MerchantClassLibraries/OrdersManager.cs
+++ MerchantClassLibraries/OrdersManager.cs
@@ -20,22 +20,24 @@ namespace MerchantClassLibraries
                 decimal generatedPrice = (decimal)rnd.Next(0, 10000000) / 100;
                 year = rnd.Next(2010, 2021);
                 month = rnd.Next(1, 13);
-                if (year % 4 == 0 && month == 2)
-                {
-                    day = rnd.Next(1, 30);
-                }
-                else if(year % 4 != 0 && month == 2)
-                {
-                    day = rnd.Next(1, 29);
-                }
-                else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+
+                if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
                 {
                     day = rnd.Next(1, 32);
                 }
-                else
+                else if (month != 2)
                 {
                     day = rnd.Next(1, 31);
                 }
+                else if (year % 4 == 0)
+                {
+                    day = rnd.Next(1, 30);
+                }
+                else
+                {
+                    day = rnd.Next(1, 29);
+                }
+                
                 DateTime generatedDate = new DateTime(year, month, day, rnd.Next(0, 24), rnd.Next(0, 60), rnd.Next(0, 60));
                 
                 orders.Add(new Order(i, generatedPrice, generatedDate, (Status)rnd.Next(0, 3)));
